73d6445b4efda4f51ed71e01eb4ba23f0d99e3ef
anu <anu_iv2002@hotmail.com>
2019-02-17 22:28:33 +0000

    ex2

---
+++ week2/oop1password.rb
@@ -0,0 +1,34 @@
+class Diary
+  def initialize
+    @contents = "Eric Cantona is the best footballer"
+  end
+
+  def read
+    @contents
+  end
+end
+
+class SecretDiary
+  def initialize
+    @diary = Diary.new
+    @unlocked = false
+  end
+
+  def unlock
+    @unlocked = true
+  end
+
+  def lock
+    @unlocked = false
+  end
+
+  def read
+    return "Go away!" unless @unlocked
+    @diary.read
+  end
+end
+
+diary = Diary.new
+secret = SecretDiary.new
+secret.lock
+puts secret.read
